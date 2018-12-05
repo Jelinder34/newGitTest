bc432dee1afc2a9d3ac8a2f2ebfc6a4fcfcd1329
Jelinder34 <jelinder34@yahoo.com>
2018-12-05 13:42:18 -0500

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Take_Two_Stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            num = Convert.ToDouble(Console.ReadLine());
+            num = num - 2;
+            if((num % 2) == 0){
+                Console.WriteLine("Bob");
+            }
+            else{
+                Console.WriteLine("Alice");
+            }
+        }
+    }
+}
